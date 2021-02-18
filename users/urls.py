36a07c54c97b5141d97b575c16ab02aa9d8e2db1
Serhii Stefanovskyi <s.stefanovskyi@gms-worldwide.com>
2021-02-18 16:23:34 +0200

    Add gjango project learning log

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+"""Определяет схемы URL для пользователей"""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Включить URL авторизации по умолчанию
+    path('', include('django.contrib.auth.urls')),
+    # Страница регистрации
+    path('register/', views.register, name='register'),
+]
